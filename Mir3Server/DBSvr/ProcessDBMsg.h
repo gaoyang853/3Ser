7ecb344a57c019b72d55944b541b34bbff5e9022
gaoyang853 <gaoyang853@163.com>
2015-01-25 18:52:51 +0800

    Revert "1111"
    This reverts commit f704afa10087604538bc5deee9de9d4ee5dc3017.

--- Mir3Server/DBSvr/ProcessDBMsg.h
+++
@@ -1,6 +0,0 @@
-
-
-#pragma once
-
-
-BOOL MakeNewItem(CServerInfo* pServerInfo, _LPTLOADHUMAN lpHumanLoad, _LPTMAKEITEMRCD lpMakeItemRcd, int nRecog);
--- Mir3Server/GameSvr/ObjectNPC.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-
-CNPCObject::CNPCObject() : CCharObject(NULL)
-{
-	m_dwRunNextTick	= 250;
-	m_dwSearchTick	= 3000;
-
-	m_btLight		= 6;
-}
